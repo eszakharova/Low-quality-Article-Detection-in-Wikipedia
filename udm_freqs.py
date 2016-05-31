74db840fd9997e72d01804b547c0ca7c61e6f1be
eszakharova <1583253@gmail.com>
2016-05-31 13:23:19 +0000

    Add files via upload

---
+++ udm_freqs.py
@@ -0,0 +1,34 @@
+import xml.dom.minidom
+import re
+
+f = open('udmwiki.xml', 'r', encoding = 'utf-8')
+text = xml.dom.minidom.parse(f)
+text = text.toprettyxml()
+pages = re.findall('<page>.*?</page>', text, flags = re.DOTALL)
+f1 = open('udmarticles2.txt', 'w', encoding = 'utf-8')
+for page in pages:
+    f1.write(page + '\n')
+f1.close()
+text = re.sub('(<(?:sitename|namespaces|comment)+>.*?<(?:/sitename|/namespaces|/comment)+>)', '', text, flags = re.DOTALL)
+text = re.sub('(\[\[:*(?:Категория|Category).*?\]\])', '', text, flags = re.DOTALL)
+text = re.sub('\[\[(Файл|файл|:Категория|Шаблон|Википедия)', '', text)
+text = re.sub('#перенаправление', '', text )
+text = re.sub('[\[\]]+', '', text)
+words = re.findall('([а-яёА-ЯЁӜӝӞӟӤӥӦӧӴӵ0-9]+-*[а-яёА-ЯЁӜӝӞӟӤӥӦӧӴӵ]*)', text)
+freq = {}
+for w in range(len(words)):
+    d = re.search('^[0-9-]+$', words[w])
+    if d is not None:
+        words[w] = ''
+    else:
+        words[w] = words[w].lower()
+        if words[w] not in freq and words[w] != '':
+            freq[words[w]] = 1
+        elif words[w] != '':
+            freq[words[w]] += 1
+sorted_keys = sorted(freq, key = lambda x:int(freq[x]), reverse = True)
+f2 = open('sorted_correct_2.txt', 'w', encoding='utf-8')
+for i in sorted_keys:
+    f2.write(i+'\t'+str(freq[i])+'\n')
+f2.close()
+
